e2e479626d032432ccc89047d7618eb919396bd8
mrkelder <vageev06@gmail.com>
2021-12-13 17:42:09 +0300

    dev: left a note about removing css layout hidding

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import MobileMenu from "./mobile/Header";
 import DesktopMenu from "./desktop/Header";
 
+// TODO: substitude css layout hidding with rendering
+
 const Header: FC = () => (
   <header className="flex items-center bg-white border-b border-grey-100 lg:border-b-0">
     <MobileMenu />
