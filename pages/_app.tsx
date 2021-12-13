bbeb39ec2bda6553f3fa58a498b54fb7288cfe4b
mrkelder <vageev06@gmail.com>
2021-12-13 18:51:51 +0300

    feat: added a global firebase provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "components/Layout";
+import { createContext } from "react";
+import Firebase from "utils/firebase";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const firebase = new Firebase();
+  const FirebaseContext = createContext<Firebase>(firebase);
+  FirebaseContext.displayName = "firebase";
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <FirebaseContext.Provider value={firebase}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </FirebaseContext.Provider>
   );
 }
 
