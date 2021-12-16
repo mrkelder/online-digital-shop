4548db87fda41bb27c7447750b08bb7dd98b6903
mrkelder <vageev06@gmail.com>
2021-12-16 20:54:17 +0300

    feat: added hover and focus state

--- components/Tab.tsx
+++ components/Tab.tsx
@@ -7,7 +7,10 @@ const Tab: FC<{ onClick: () => void; showIcon?: boolean; name: string }> = ({
   name
 }) => {
   return (
-    <button className="flex py-3 px-5 items-center w-full" onClick={onClick}>
+    <button
+      className="flex py-3 px-5 items-center w-full transition-colors hover:bg-grey-100 focus:bg-grey-100"
+      onClick={onClick}
+    >
       <span className="mr-5 text-xs">icon</span>
       <p className="flex-1 text-left text-base">{name}</p>
       {showIcon && (
