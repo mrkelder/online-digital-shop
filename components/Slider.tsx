214d0fdee06a67a8d646200114542665bd4e5cae
Vlad A <vageev06@gmail.com>
2021-12-21 10:01:39 +0300

    feat: created custom pagination

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,11 +1,20 @@
 import { FC } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperProps, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper";
+import "swiper/css/pagination";
 import "swiper/css";
 
 const Slider: FC<{ slides: string[] }> = ({ slides }) => {
+  const swiperParams: SwiperProps = {
+    slidesPerView: 1,
+    modules: [Autoplay, Pagination],
+    autoplay: { disableOnInteraction: false, delay: 4500 },
+    pagination: { clickable: true }
+  };
+
   return (
-    <div className="my-2 w-full">
-      <Swiper slidesPerView={1}>
+    <div className="my-2 relative flex justify-center w-full lg:my-0 lg:max-w-7xl lg:mx-auto">
+      <Swiper {...swiperParams}>
         {slides.map(i => (
           <SwiperSlide key={i}>
             <img src={i} className="mx-auto" />
