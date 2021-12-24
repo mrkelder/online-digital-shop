db80f21ad1105bc8b8a66e41d8a18e8d8eaab5b1
Vlad A <vageev06@gmail.com>
2021-12-24 16:42:13 +0300

    feat: added an "advantages" banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,19 @@ import Slider from "components/Slider";
 import Head from "next/head";
 import type { GetServerSideProps, NextPage } from "next";
 import Firebase, { FirebaseContext } from "utils/firebase";
+import GuaranteeIcon from "public/img/guarantee.svg";
+import LikeIcon from "public/img/like.svg";
+import TruckIcon from "public/img/truck.svg";
 
 type SlideNames = { slideNames: string[] };
 
 const DESKTOP_SLIDE_BREAKPOINT = 1024;
+
+const advantages = [
+  { img: LikeIcon, text: "Пополнение счета без комиссии" },
+  { img: TruckIcon, text: "Бесплатная доставка в магазины" },
+  { img: GuaranteeIcon, text: "Официальная гарантия от производителя" }
+];
 
 const Home: NextPage<SlideNames> = ({ slideNames }) => {
   const firebase = useContext(FirebaseContext);
@@ -31,6 +40,17 @@ const Home: NextPage<SlideNames> = ({ slideNames }) => {
       </Head>
 
       <Slider {...{ slides }} />
+
+      <ul className="hidden lg:px-2 lg:py-5 lg:justify-center lg:space-x-10 lg:flex lg:bg-grey-400 lg:text-white w-full">
+        {advantages.map(I => (
+          <li className="flex flex-col items-center" key={I.text}>
+            <span className="w-8">
+              <I.img />
+            </span>
+            <p className="font-regular w-48 text-center">{I.text}</p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
