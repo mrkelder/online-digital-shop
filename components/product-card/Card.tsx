f92a886e6abb345655ca39c3a9709b8d3e876d59
Vlad A <vageev06@gmail.com>
2022-01-03 09:58:27 +0300

    feat: made item card alive

--- components/product-card/Card.tsx
+++ components/product-card/Card.tsx
@@ -13,16 +13,22 @@ interface Props {
   photo: StaticImageData | string;
 }
 
-const LINK =
-  "https://www.vodafone.ua/shop/media/catalog/product/cache/b194688dfba11ab51374da7d8ad52d29/1/3/134640-samsung-galaxy-s20-fe-2021-6-128gb-sm-g780glvhsek-blue.jpg";
-
 const Card: FC<Props> = ({ rating, price, name, photo, id }) => {
+  const link = `/products/${id}`;
+
   return (
     <div className="flex flex-col shadow-lg my-10 bg-white w-60 px-3 py-5 text-grey-300">
-      <img src={LINK} alt="test" className="h-48 object-contain mb-2" />
+      <div className="relative h-48 mb-2">
+        <Image
+          src={photo}
+          alt="Фотография товара"
+          objectFit="contain"
+          layout="fill"
+        />
+      </div>
 
       <div className="relative inline h-12 overflow-hidden">
-        <Link href="/somelink">
+        <Link href={link}>
           <a className="text-sm">{name}</a>
         </Link>
         <div className="absolute w-full h-3 bottom-0 left-0 white-shadow" />
@@ -43,7 +49,7 @@ const Card: FC<Props> = ({ rating, price, name, photo, id }) => {
 
       <span className="text-red my-1">{price} грн</span>
 
-      <Link href={LINK}>
+      <Link href={link}>
         <a className="bg-red text-white text-sm p-2 mt-1 text-center">
           Детальнее
         </a>
