26a862e369939455e56c63dce71b25dfbc34f1c5
Vlad A <vageev06@gmail.com>
2022-01-08 20:14:33 +0300

    feat: added city search

--- components/map/ShopMap.tsx
+++ components/map/ShopMap.tsx
@@ -1,13 +1,42 @@
-import { FC, useState } from "react";
+import { FC, useState, FormEventHandler } from "react";
 import GMap from "./GMap";
 import styles from "styles/map.module.css";
 
+interface SearchInfo {
+  chosenCityId: City["id"];
+  searchResults: City[];
+}
+
 const ShopMap: FC<{ geoInfo: GeoInfo }> = ({ geoInfo }) => {
   const [showShops, setShowShops] = useState(false);
+  // FIXME: replace useState with useReducer
+  const [searchInfo, setSearchInfo] = useState<SearchInfo>({
+    chosenCityId: geoInfo.cities[0].id,
+    searchResults: geoInfo.cities
+  });
+
+  const { searchResults, chosenCityId } = searchInfo;
+  const resultShops = geoInfo.shops.filter(i => i.city === chosenCityId);
   const shopListStyle = showShops ? "flex" : "hidden";
 
   const toggleShopList = () => {
     setShowShops(!showShops);
+  };
+
+  const handleSearch: FormEventHandler<HTMLInputElement> = event => {
+    const { value } = event.target as HTMLInputElement;
+    const regEx = new RegExp("^" + value, "i");
+    const resultCities = geoInfo.cities.filter(i => i.name.match(regEx));
+
+    resultCities.length > 0
+      ? setSearchInfo({ ...searchInfo, searchResults: resultCities })
+      : setSearchInfo({ ...searchInfo, searchResults: geoInfo.cities });
+  };
+
+  const chooseCity = (cityId: City["id"]) => {
+    return () => {
+      setSearchInfo({ ...searchInfo, chosenCityId: cityId });
+    };
   };
 
   return (
@@ -18,12 +47,14 @@ const ShopMap: FC<{ geoInfo: GeoInfo }> = ({ geoInfo }) => {
             type="text"
             placeholder="Город"
             className={styles["map-search-input"]}
+            onInput={handleSearch}
           />
           <div className="space-y-2 px-2 py-2 w-full overflow-y-auto border border-grey-400">
-            {geoInfo.cities.map(i => (
+            {searchResults.map(i => (
               <button
                 key={i.id}
                 className="font-light text-sm w-full text-left"
+                onClick={chooseCity(i.id)}
               >
                 {i.name}
               </button>
@@ -38,7 +69,7 @@ const ShopMap: FC<{ geoInfo: GeoInfo }> = ({ geoInfo }) => {
           shopListStyle
         }
       >
-        {geoInfo.shops.map(i => (
+        {resultShops.map(i => (
           <button key={i.id}>{i.name}</button>
         ))}
       </div>
