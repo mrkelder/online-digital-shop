2dc79d1f62eafd75e1c5d8d2c12948bc5f704a60
Vlad A <vageev06@gmail.com>
2022-01-22 10:18:40 +0300

    feat: empty cart page (desktop)

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -5,15 +5,17 @@ const CartPage: NextPage = () => {
   return (
     <div className="py-3 px-3">
       <Link href="/catalog">
-        <a className="underline">Продолжить покупки</a>
+        <a className="underline lg:text-base">Продолжить покупки</a>
       </Link>
       <div className="flex flex-col items-center">
-        <h1 className="mt-2">Корзина</h1>
+        <h1 className="mt-2 lg:mt-4 lg:mb-2">Корзина</h1>
         <p className="text-grey-300 text-base">
           0 товаров на сумму{" "}
           <span className="text-grey-650 font-bold text-base">0 грн</span>
         </p>
-        <p className="mt-3 text-base">Корзина пуста</p>
+        <p className="mt-3 text-base lg:mt-16 lg:text-lg lg:font-bold">
+          Корзина пуста
+        </p>
       </div>
     </div>
   );
