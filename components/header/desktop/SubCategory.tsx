2976f47845eec8b1caff99fc6612783f9fbeb1d5
Vlad A <vageev06@gmail.com>
2022-02-03 13:06:54 +0300

    fix: now catalog closes when clicking subcategory link on desktop

--- components/header/desktop/SubCategory.tsx
+++ components/header/desktop/SubCategory.tsx
@@ -2,9 +2,17 @@ import { FC } from "react";
 import Link from "next/link";
 
 const SubCategory: FC<{ subCategory: SubCategory }> = ({ subCategory }) => {
+  const closeCatalog = () => {
+    const event = new Event("close-catalog");
+    dispatchEvent(event);
+  };
+
   return (
     <Link href={`/catalog?id=${subCategory.id}`}>
-      <a className="w-1/4 border-b-2 text-grey-600 border-grey-100 ont-light text-xl font-semibold">
+      <a
+        onClick={closeCatalog}
+        className="w-1/4 border-b-2 text-grey-600 border-grey-100 ont-light text-xl font-semibold"
+      >
         {subCategory.name}
       </a>
     </Link>
