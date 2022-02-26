0fce8b487846c645f1bf44159db944e53498233a
Vlad A <vageev06@gmail.com>
2022-02-26 18:59:02 +0300

    fix: extracted firebase data to .env.local file

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -21,13 +21,13 @@ class Firebase {
   private readonly storage: FirebaseStorage;
 
   static firebaseConfig = {
-    apiKey: "AIzaSyBrHoU08YA0Q7lBEAWyc9Ld4Fze0R_O8w4",
-    authDomain: "new-london-8d2c3.firebaseapp.com",
-    projectId: "new-london-8d2c3",
-    storageBucket: "new-london-8d2c3.appspot.com",
-    messagingSenderId: "173083750309",
-    appId: "1:173083750309:web:b7fadcf6afdd79e257c24c",
-    measurementId: "G-SSZS19QCH8"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
   };
 
   constructor() {
