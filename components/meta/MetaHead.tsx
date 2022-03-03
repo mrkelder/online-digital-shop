ae6528fb12cc58347d401d1ec9e9bfcf200ca00d
Vlad A <vageev06@gmail.com>
2022-03-03 12:40:18 +0300

    feat: added open graph and twitter meta tags

--- components/meta/MetaHead.tsx
+++ components/meta/MetaHead.tsx
@@ -7,17 +7,37 @@ interface Props {
   keywords?: string;
   description?: string;
   author?: string;
+  image?: string;
 }
 
 const SITE_NAME = "New London";
 
-const MetaHead: FC<Props> = ({ title, keywords, description, author }) => {
+const MetaHead: FC<Props> = ({
+  title,
+  keywords,
+  description,
+  author,
+  image
+}) => {
   return (
     <Head>
       <title>{title}</title>
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta name="author" content={author} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={image} />
+      <meta property="og:url" content="/" />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={title} />
+
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image:alt" content="Логотип" />
     </Head>
   );
 };
@@ -28,7 +48,8 @@ MetaHead.defaultProps = {
     "Бытовая техника, мобильные телефоны, телевизоры, кондиционеры, холодильники, мониторы, ноутбуки с доставкой",
   description:
     "Бытовая техника, мобильные телефоны, телевизоры, кондиционеры, холодильники, мониторы, ноутбуки с доставкой",
-  author: SITE_NAME
+  author: SITE_NAME,
+  image: "/logo_cover.png"
 };
 
 export default MetaHead;
