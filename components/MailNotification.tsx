490a9092e8530265c2f123892f591553d8382696
Vlad A <vageev06@gmail.com>
2022-03-05 11:24:18 +0300

    fix: removed image warning in MailNotification

--- components/MailNotification.tsx
+++ components/MailNotification.tsx
@@ -9,8 +9,8 @@ import Input from "./Input";
 
 const MailNotification: FC = () => {
   return (
-    <div className="py-10 bg-grey-500 flex items-center justify-center relative sm:justify-start">
-      <div className="absolute w-full h-full opacity-40">
+    <div className="h-64 bg-grey-500 flex items-center justify-center relative sm:justify-start lg:h-56">
+      <div className="relative w-full h-full opacity-40">
         <Image
           src={newsletterIcon}
           alt="Новости"
@@ -19,15 +19,17 @@ const MailNotification: FC = () => {
           objectPosition="100%"
         />
       </div>
-      <div className="w-3/4 relative max-w-sm ml-3.5 lg:ml-10 lg:max-w-max">
-        <h2 className="text-white text-center mb-2">
-          Следите за последними новостями и акциями
-        </h2>
-        <div className="lg:max-w-sm">
-          <Input type="email" placeholder="Email" />
-        </div>
-        <div className="mt-2 lg:max-w-sm">
-          <Button variant="lg">Подписаться</Button>
+      <div className="absolute w-3/4 max-w-sm ml-3.5 lg:ml-10 lg:max-w-max">
+        <div className="relative">
+          <h2 className="text-white text-center mb-2">
+            Следите за последними новостями и акциями
+          </h2>
+          <div className="lg:max-w-sm">
+            <Input type="email" placeholder="Email" />
+          </div>
+          <div className="mt-2 lg:max-w-sm">
+            <Button variant="lg">Подписаться</Button>
+          </div>
         </div>
       </div>
     </div>
