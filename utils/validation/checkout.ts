e421111201b47dc4149b40ef345400294cd0f7a9
Vlad A <vageev06@gmail.com>
2022-03-10 11:10:21 +0300

    fix: removed unused card data fields

--- utils/validation/checkout.ts
+++ utils/validation/checkout.ts
@@ -4,38 +4,21 @@ export interface FormData {
   street: string;
   house: string;
   apartment: string;
-  number: string;
-  date: string;
-  pin: string;
   email: string;
 }
 
 const FULLNAME_REG =
   /^ *[A-Za-zА-Яа-яёЁЇїІіЄєҐґ]{1,} +[A-Za-zА-Яа-яёЁЇїІіЄєҐґ]{1,} *$/;
-const EXPIRATIONAL_DATE_REG = /\d{2}\/\d{2}/;
 const EMAIL_REG =
   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-function removeSpaces(str: string) {
-  return str.replace(/ /g, "");
-}
-
-function includesOnlyNumbers(str: string) {
-  return /^[0-9]+$/g.test(removeSpaces(str));
-}
-
 export function validateFormData(obj: FormData): keyof FormData | undefined {
-  const { fullName, house, city, email, apartment, number, date, pin, street } =
-    obj;
+  const { fullName, house, city, email, apartment, street } = obj;
   if (!FULLNAME_REG.test(fullName)) return "fullName";
   if (apartment.length < 1) return "apartment";
   if (house.length < 1) return "house";
   if (city.length < 1) return "city";
   if (street.length < 1) return "street";
-  if (!includesOnlyNumbers(number) || removeSpaces(number).length !== 16)
-    return "number";
-  if (!EXPIRATIONAL_DATE_REG.test(date)) return "date";
-  if (!includesOnlyNumbers(pin) || removeSpaces(pin).length !== 3) return "pin";
   if (!EMAIL_REG.test(email)) return "email";
 }
 
