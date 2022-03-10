0c8376aaf6be4d06e87c14c9a3fb270c4e9d85c8
Vlad A <vageev06@gmail.com>
2022-03-10 10:24:44 +0300

    fix: removed bottom border on StageWrapper's heading

--- components/checkout-page/StageWrapper.tsx
+++ components/checkout-page/StageWrapper.tsx
@@ -9,10 +9,11 @@ interface Props {
 const StageWrapper: FC<Props> = ({ children, stageNumber, active, title }) => {
   const contentVisibilityStyling = active ? "block" : "hidden";
   const headingColorStyling = active ? "red" : "grey-300";
+  const headingBorderStyling = active ? "border-b border-grey-100" : "";
 
   return (
     <div className="bg-white shadow-md border border-grey-100">
-      <div className="flex items-center p-2 border-b border-grey-100 lg:p-3">
+      <div className={`flex items-center p-2 ${headingBorderStyling} lg:p-3`}>
         <p
           className={`flex items-center justify-center w-8 h-8 rounded-full bg-${headingColorStyling} text-white text-xl mr-2 lg:w-12 lg:h-12 lg:mr-3`}
         >
