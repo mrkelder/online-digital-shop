7075dfebaa2c5d7a57f06cc21fcaee5e2d0c2789
Vlad A <vageev06@gmail.com>
2022-04-04 14:48:53 +0300

    fix: added a warning message when no city is loaded

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -6,6 +6,7 @@ import ShopMap from "./ShopMap";
 
 const Map: FC<MapProps> = ({ cities }) => {
   if (cities.length === 0) {
+    console.warn("No city was loaded");
     return (
       <div className="w-full bg-grey-500 text-white text-xl h-86 flex items-center justify-center text-center px-2 lg:h-130">
         Возникла проблема при загрузке карты
