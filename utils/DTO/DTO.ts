4d7128406d250638c9ef3836e990840936c974f1
Vlad A <vageev06@gmail.com>
2022-04-12 09:11:57 +0300

    fix: removed unused firebase

--- utils/DTO/DTO.ts
+++ utils/DTO/DTO.ts
@@ -6,19 +6,6 @@ class DTO {
     return { _id, name, price, photo, quantity: 1 };
   }
 
-  public static categoriesToSubCategoryIds(categories: Category[]) {
-    const subcategoryIds: SubCategory["id"][] = [];
-    categories.forEach(i =>
-      i.subcategories.forEach(s => subcategoryIds.push(s))
-    );
-    return subcategoryIds;
-  }
-
-  public static firebaseShopToShop(shop: FirebaseShop): Shop {
-    const { _lat, _long } = shop.geo;
-    return { ...shop, geo: { lat: _lat, lng: _long } };
-  }
-
   public static mongodbCharacteristicValueToString(
     values: string[],
     indexes: number[]
