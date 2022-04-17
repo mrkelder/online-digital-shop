87e1b7214bf1c5e543f0da2ee2a14bca4da78271
Vlad A <vageev06@gmail.com>
2022-04-17 16:17:25 +0300

    feat: localized MailNotification component

--- components/MailNotification.tsx
+++ components/MailNotification.tsx
@@ -2,18 +2,21 @@ import { FC } from "react";
 
 import Image from "next/image";
 
+import useLanguage from "hooks/useLanguage";
 import newsletterIcon from "public/img/newsletter.webp";
 
 import Button from "./Button";
 import Input from "./Input";
 
 const MailNotification: FC = () => {
+  const { langVariant } = useLanguage();
+
   return (
     <div className="h-64 bg-grey-500 flex items-center justify-center relative sm:justify-start lg:h-56">
       <div className="relative w-full h-full opacity-40">
         <Image
           src={newsletterIcon}
-          alt="Новости"
+          alt={langVariant("Новини", "Новости")}
           layout="fill"
           objectFit="contain"
           objectPosition="100%"
@@ -22,13 +25,18 @@ const MailNotification: FC = () => {
       <div className="absolute w-3/4 max-w-sm ml-3.5 lg:ml-10 lg:max-w-max">
         <div className="relative">
           <h2 className="text-white text-center mb-2">
-            Следите за последними новостями и акциями
+            {langVariant(
+              "Слідкуйте за останніми новинами та акціями",
+              "Следите за последними новостями и акциями"
+            )}
           </h2>
           <div className="lg:max-w-sm">
             <Input type="email" placeholder="Email" />
           </div>
           <div className="mt-2 lg:max-w-sm">
-            <Button variant="lg">Подписаться</Button>
+            <Button variant="lg">
+              {langVariant("Подписаться", "Подписаться")}
+            </Button>
           </div>
         </div>
       </div>
