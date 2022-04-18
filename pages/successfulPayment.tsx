9acaf882310f24e9fb7ca81d9b28a0b60ca46f68
Vlad A <vageev06@gmail.com>
2022-04-18 14:58:59 +0300

    feat: now cart is cleaned up after a successful payment

--- pages/successfulPayment.tsx
+++ pages/successfulPayment.tsx
@@ -1,12 +1,21 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
 import Link from "next/link";
+import { useDispatch } from "react-redux";
 
+import { AMOUNT_OF_ITEMS_IN_CART } from "constants/cookie-names";
 import useLanguage from "hooks/useLanguage";
 import SuccessIcon from "public/img/success.svg";
+import Cookie from "utils/Cookie";
 
 const SuccessfulPaymentPage: FC = () => {
   const { langVariant } = useLanguage();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: "cart/restore" });
+    Cookie.setCookie(AMOUNT_OF_ITEMS_IN_CART, "0");
+  }, [dispatch]);
 
   return (
     <div className="flex flex-col items-center">
